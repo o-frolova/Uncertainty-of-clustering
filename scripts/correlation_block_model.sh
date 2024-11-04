a037e24ecb0114e01d34c28d28a717d25d69df32
o-frolova <olga.frolova.01@yandex.ru>
2024-11-04 22:14:43 +0300

    refactoring script

--- scripts/correlation_block_model.sh
+++ scripts/correlation_block_model.sh
@@ -4,21 +4,20 @@ source env/bin/activate
 export PYTHONPATH=/Users/olgafrolova/Documents/ЛАТАС/code
 set -ex
 
-OUTPUT_DIR=./data/results/CorrelationBlockModel/
+PATH_TO_SAVE=./data/results/CorrelationBlockModel/
 FILE_NAME=correlation_block_model_all_results.csv
 NUMBER_VERTICES=60
 NUMBER_REPETITIONS=100
 R_IN=0.8
 R_OUT=0.1
 
-
 echo "Running experiment..."
 python scripts/correlation_block_model.py \
-    --path-to-save ${OUTPUT_DIR} \
+    --path-to-save ${PATH_TO_SAVE} \
     --name-common-file ${FILE_NAME} \
     --number-vertices ${NUMBER_VERTICES} \
     --number-repetitions ${NUMBER_REPETITIONS} \
     --r-in ${R_IN} \
     --r-out ${R_OUT}
 
-echo "Experiment completed. Results saved in ${OUTPUT_DIR}"
+echo "Experiment completed. Results saved in ${PATH_TO_SAVE}"
