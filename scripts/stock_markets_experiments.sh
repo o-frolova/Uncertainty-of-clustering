b6e38162f33d04739048f79be0c15c2d92fd6104
o-frolova <olga.frolova.01@yandex.ru>
2024-11-04 22:11:20 +0300

    add script

---
+++ scripts/stock_markets_experiments.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source env/bin/activate
+export PYTHONPATH=/Users/olgafrolova/Documents/ЛАТАС/code
+set -ex
+
+START_DATE=2016-01-01
+END_DATE=2018-12-31
+PATH_TO_DATA=./data/DataStocks/SP100
+PATH_TO_SAVE=./data/results/SP100_60_stocks_41124/
+NAME_COMMON_FILE=SP100_60_stocks_all_results_test.csv
+NUMBER_STOCKS=60
+NUMBER_REPETITIONS=100
+
+echo "Running experiment..."
+python ./scripts/stock_markets_experiment.py \
+  --start-date ${START_DATE} \
+  --end-date ${END_DATE} \
+  --path-to-data ${PATH_TO_DATA} \
+  --path-to-save ${PATH_TO_SAVE} \
+  --name-common-file ${NAME_COMMON_FILE} \
+  --number-stocks ${NUMBER_STOCKS} \
+  --number-repetitions ${NUMBER_REPETITIONS}
+
+echo "Experiment completed. Results saved in ${PATH_TO_SAVE}"
